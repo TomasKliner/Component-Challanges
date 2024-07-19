a23e308509ad9c5a7da965bff516dc88176374ba
TomasKliner <tomasklinerr@gmail.com>
2024-07-19 16:26:47 +0200

    first section styling draft

--- app/mortage-calculator/page.tsx
+++ app/mortage-calculator/page.tsx
@@ -1,37 +1,91 @@
+import "./variables.css";
+import styles from "./styles.module.css";
 export default function MortageCalculator() {
   return (
-    <div>
-      <div id="left">
-        <h1>Mortgage Calculator</h1>
-        <span>Clear All</span>
+    <div className={styles.body}>
+      <div className={styles.first}>
+        <h1 className={styles.h1}>Mortgage Calculator</h1>
+        <div className={styles.clear}>Clear All</div>
 
-        <label>Mortgage Amount</label>
+        <section className={styles.section}>
+          <label className={styles.label} htmlFor="mortage">
+            Mortgage Amount
+          </label>
+          <input
+            id="mortage"
+            className={styles.input}
+            type="text"
+            placeholder=""
+          />
+        </section>
+        <section className={styles.section}>
+          <label className={styles.label} htmlFor="term">
+            Mortgage Term
+          </label>
+          <input
+            id="term"
+            className={styles.input}
+            type="text"
+            placeholder=""
+          />
+        </section>
+        <section className={styles.section}>
+          <label className={styles.label} htmlFor="interest">
+            Interest Rate
+          </label>
+          <input
+            id="interest"
+            className={styles.input}
+            type="text"
+            placeholder=""
+          />
+        </section>
 
-        <label>Mortgage Term</label>
-
-        <label>Interest Rate</label>
-
-        <div>
-          <label htmlFor="radio-group">Mortgage Type</label>
-          <div id="radio-group" className="radio-group">
-            <input
-              type="radio"
-              id="repayment"
-              name="mortgage-type"
-              defaultValue="repayment"
-            />
-            <label htmlFor="repayment">Repayment</label>
-            <input
-              type="radio"
-              id="interest-only"
-              name="mortgage-type"
-              defaultValue="interest-only"
-            />
-            <label htmlFor="interest-only">Interest Only</label>
+        <section className={styles.section}>
+          <label className={styles.label} htmlFor="radio-group">
+            Mortgage Type
+          </label>
+          <div id="radio-group" className={styles.section}>
+            <div className={styles.mortageType}>
+              <input
+                type="radio"
+                id="repayment"
+                name="mortgage-type"
+                defaultValue="repayment"
+              />
+              <label className={styles.label} htmlFor="repayment">
+                Repayment
+              </label>
+            </div>
+            <div className={styles.mortageType}>
+              <input
+                type="radio"
+                id="interest-only"
+                name="mortgage-type"
+                defaultValue="interest-only"
+              />
+              <label className={styles.label} htmlFor="interest-only">
+                Interest Only
+              </label>
+            </div>
           </div>
-        </div>
+        </section>
 
-        <button>Calculate Repayments</button>
+        <button className={styles.submit}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#133041"
+              d="M18.75 2.25H5.25a1.5 1.5 0 0 0-1.5 1.5v16.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V3.75a1.5 1.5 0 0 0-1.5-1.5Zm-10.5 16.5a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 18.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 15a1.125 1.125 0 1 1 0-2.25A1.125 1.125 0 0 1 12 15Zm3.75 3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm1.5-5.25a.75.75 0 0 1-.75.75h-9a.75.75 0 0 1-.75-.75V6a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 .75.75v3.75Z"
+            />
+          </svg>
+          <span>Calculate Repayments</span>
+        </button>
       </div>
 
       {/* <!-- Empty results start --> */}
@@ -43,9 +97,9 @@ export default function MortageCalculator() {
         monthly repayments would be.
       </p>
 
-      {/* <!-- Empty results end -->
+      {/* <!-- Empty results end -->*/}
 
-<!-- Completed results start --> */}
+      {/*<!-- Completed results start --> */}
 
       <h2>Your results</h2>
 
@@ -67,7 +121,9 @@ export default function MortageCalculator() {
         <a href="https://www.frontendmentor.io?ref=challenge">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Kliner Tomas</a>.
+        <span>
+          . Coded by <a href="https://kliner.eu">Kliner Tomas</a>.
+        </span>
       </div>
     </div>
   );
